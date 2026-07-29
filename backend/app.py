681e3ed70c69b9ca5d011147cf692499243153bb
Zeinab Azordeh <z.azordeh2006@gmail.com>
2026-07-29 13:26:50 +0330

    sync

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
-from routes import auth_router  # ۱. ایمپورت کردن روتری که ساختی
+from routes import auth_router
 
-app = FastAPI(
-    title="Sport Ticket Reservation System",
-    description="Backend API for sport ticket reservation project",
-    version="1.0.0"
-)
+app = FastAPI()
 
-# ۲. متصل کردن روتر احراز هویت به برنامه اصلی (این خط خیلی مهم است!)
 app.include_router(auth_router.router)
 
 @app.get("/")
 def root():
-    return {
-        "message": "Sport Ticket Reservation API is running"
-    }
+    return {"message": "API Server is running!"}
