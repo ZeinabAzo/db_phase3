70cc0a6f0d7602f62a86721ab2a5a967f11f38f1
erfan <erfanmmgz@gmail.com>
2026-07-30 12:20:15 -0700

    Save point: current working version

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routes.location import router as location_router
+from routes.ticket import router as ticket_router
 from routes import auth_router
 from routes.user_router import router as user_router
+
 
 #  create api instance 
 app = FastAPI(
@@ -13,6 +15,8 @@ app = FastAPI(
 app.include_router(location_router)
 app.include_router(auth_router.router)
 app.include_router(user_router)
+app.include_router(ticket_router)
+
 
 # first test
 @app.get("/")
@@ -20,9 +24,3 @@ def root():
     return {
         "message": "Sport Ticket Reservation API is running"
     }
-
-
-
-
-
-
