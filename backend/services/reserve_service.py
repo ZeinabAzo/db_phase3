70af6f3c7512adb5fb44f2c9d251fa6699e59c3f
mahdie <davoudzademahdie@gmail.com>
2026-07-30 19:40:57 +0330

    Add active reservations API

--- backend/services/reserve_service.py
+++ backend/services/reserve_service.py
@@ -34,13 +34,13 @@ def reserve_ticket(user_id, ticket_id):
             }
 
         created_at = datetime.now()
-        expired_at = created_at + timedelta(minutes = 10)
+        expire_at = created_at + timedelta(minutes = 10)
 
         cursor.execute(
             """
             insert into reserve(user_id, ticket_id, total_price, status, created_at, expire_at)
             values(%s,%s,%s,%s,%s,%s) """
-            ,(user_id, ticket_id, ticket["price"],"pending", created_at, expired_at),
+            ,(user_id, ticket_id, ticket["price"],"pending", created_at, expire_at),
         )
 
         cursor.execute(
@@ -67,5 +67,56 @@ def reserve_ticket(user_id, ticket_id):
         if cursor:
             cursor.close()
 
+        if "data_connection" in locals():
+            data_connection.close()
+
+
+
+def active_reservations(user_id):
+    cursor = None
+
+    try:
+        data_connection = get_connection()
+        cursor = data_connection.cursor(dictionary = True)
+
+        cursor.execute(
+            """
+            select r.reserve_id, r.ticket_id, r.total_price, r.created_at, r.expire_at, r.status,
+                   t.price,
+                   m.match_id, m.start_time , 
+                   home.name as home_team, away.name as away_team
+
+            from reserve r
+
+            inner join ticket t
+            on r.ticket_id = t.ticket_id
+
+            inner join `match` m
+            on t.match_id = m.match_id
+
+            inner join team home
+            on m.home_team_id = home.team_id
+
+            inner join team away
+            on m.away_team_id = away.team_id
+
+            where r.user_id = %s and r.status = 'pending' and r.expire_at > now()
+            order by r.created_at desc """, (user_id,)
+        )
+
+        reservations = cursor.fetchall()
+
+        return {"success" : True, "data" : reservations}
+
+
+
+    except Exception as e:
+        return{
+            "success" : False, "message" : "failde to get active reservation"
+        }
+
+    finally:
+        if cursor:
+            cursor.close()
         if "data_connection" in locals():
             data_connection.close()
